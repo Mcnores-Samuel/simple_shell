fb783b1ebbaa9297023d0f29c0bc9c014c237dd5
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-05-23 00:33:28 +0200

    looks for built-in commands

---
+++ find_built-in.c
@@ -0,0 +1,30 @@
+#include "simple_shell.h"
+
+/**
+ * func_ptr - looks any funtion matching str.
+ * @str: input string to look for.
+ * Return: pointer to a function that takes a string
+ * argv as input and returns void
+ *
+*/
+
+void (*func_ptr(char *str))(char *)
+{
+	int i = 0;
+
+	built_in cmd[] = {
+		{"exit", _exit_process},
+		{"env", env},
+		{NULL, NULL}
+	};
+
+	while (cmd[i].cmdname != NULL)
+	{
+		if (_strcmp(cmd[i].cmdname, str) == 0)
+		{
+			return (cmd[i].function);
+		}
+		i++;
+	}
+	return (NULL);
+}
