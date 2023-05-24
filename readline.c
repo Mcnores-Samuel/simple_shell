09060602ec1341c65cc5f7e5886ac419a9a9d057
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-05-24 17:46:22 +0200

    fixed the jump condition

--- readline.c
+++ readline.c
@@ -1,91 +1,41 @@
 #include "simple_shell.h"
 
 /**
- * remove_reading_space - removes extra space characters
- * @buffer: double pointer to the location reading characters from
- * @line: pointer to the location writing the cleaned characters
- * Return: void
- */
-
-int remove_reading_space(char **buffer, char *line)
-{
-	int i, n, count;
-
-	for (i = 0; i < _strlen(*buffer); i++)
-		if ((*buffer)[i] != ' ' && (*buffer)[i] != '\0')
-			break;
-
-	if ((*buffer)[i] == '\0')
-	{
-		return (0);
-	}
-	for (n = 0; (*buffer)[i] != '\0'; n++)
-	{
-		if ((*buffer)[i] == ' ')
-		{
-			if ((*buffer)[i + 1] == ' ')
-			{
-				line[n] = (*buffer)[i];
-				for (count = i + 1; (*buffer)[count] != '\0'; count++)
-				{
-					if ((*buffer)[count] != ' ')
-					{
-						n++;
-						break;
-					}
-				}
-				i = count;
-				line[n] = (*buffer)[i];
-			}
-		}
-		line[n] = (*buffer)[i];
-		i++;
-	}
-	free(*buffer);
-	line[n] = '\0';
-	return (_strlen(line));
-}
-
-/**
  * _readline - reads input from the command line.
- * @line: pointer to an array to hold thinput line.
+ * @line: pointer to an array to hold the input line.
  * Return: number of bytes read or -1 on failure.
  */
-
 int _readline(char *line)
 {
-	char character = '\0', *buffer;
-	int i = 0;
-	int char_read, size = 10;
+	char characters[1024];
+	int i = 0, n = 0;
+	int char_read;
+	
+	char_read = read(STDIN_FILENO, &characters, 1024);
+	if (char_read == 0 && char_read < 0)
+		return (-1);
+	characters[char_read] = '\0';
 
-	buffer = (char *)malloc(sizeof(char) * size);
-	while (character != EOF && character != 10)
+	for (i = 0; i < _strlen(characters); i++)
+		if (characters[i] != ' ')
+			break;
+
+	while (characters[i] != 10)
 	{
-		char_read = read(STDIN_FILENO, &character, 1);
-
-		if (char_read == 0 || char_read < 0)
-		{
+		if (characters[i] == EOF || characters[i] == '\0')
 			return (-1);
-		}
-
-		if (i >= size)
+		if (characters[i] != 10)
 		{
-			size += 5;
-			buffer = realloc(buffer, size);
-		}
-
-		if (character != 10)
-		{
-			buffer[i] = character;
+			line[n] = characters[i];
 			i++;
+			n++;
 		}
 		else
 		{
 			break;
 		}
 	}
-	buffer[i] = '\0';
-	i = 0;
-	i = remove_reading_space(&buffer, line);
+	line[i] = '\0';
+
 	return (i);
 }
